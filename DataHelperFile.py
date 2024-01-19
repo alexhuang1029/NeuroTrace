0cbba9feb4efaaddd3a15499047f25b5427bb160
alexhuang1029 <103464616+alexhuang1029@users.noreply.github.com>
2024-01-19 13:49:14 -0700

    Update DataHelperFile.py

--- DataHelperFile.py
+++ DataHelperFile.py
@@ -22,4 +22,8 @@ def summary(hdata):
   print(f'Total Number of Duplicated Data within DataFrame: {hdata[hdata.duplicated()].sum().sum()}')
   print('-' * 15)
   print(f"Total Number of Unique Data within DataFrame: {hdata.stack().nunique()}")
-## hello!!
+
+def dataQuality(hdata):
+  def data_quality_numeric(hdata):
+    numeric_df = hdata(['air_time4','disp_index4','gmrt_in_air4','gmrt_on_paper4','max_x_extension4','max_y_extension4','mean_acc_in_air4','mean_acc_on_paper4','mean_gmrt4','mean_jerk_in_air4','mean_jerk_on_paper4','mean_speed_in_air4','mean_speed_on_paper4','num_of_pendown4','paper_time4','pressure_mean4','pressure_var4','total_time4']).describe(percentiles=[.25, .5, .75]).T # drop the ID column
+    return numeric_df
